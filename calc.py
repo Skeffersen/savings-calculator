8892b3adadcbf3b928a48505e1a6760b5b6690e7
Derek Kadwell <derek.kadwell@gmail.com>
2014-10-29 19:09:16 -0600

    added a clean up script changed my output function

--- calc.py
+++ calc.py
@@ -1,22 +1,39 @@
 import sqlite3
 
-def create_database():
-    db = sqlite3.connect('savings.db')
+def create_account(account_name='saving', initial_deposit=0):
+    db = sqlite3.connect('Bank.db')
 
     cursor = db.cursor()
-    cursor.execute('CREATE TABLE savings(total INTEGER)')
+    cursor.execute('CREATE TABLE IF NOT EXISTS Transactions(id INTEGER PRIMARY KEY, account_id INTEGER, type TEXT, amount INTEGER)')
+    cursor.execute('INSERT INTO Transactions(account_id, type, amount)VALUES(1, ?, ?)', ('Deposit', initial_deposit))
+    cursor.execute('CREATE TABLE IF NOT EXISTS Accounts(id INTEGER PRIMARY KEY, name TEXT)')
+    cursor.execute('INSERT INTO Accounts(name)VALUES(?)', (account_name,))
     db.commit()
 
     db.close()
 
-def deposit():
-    db = sqlite3.connect('savings.db')
+def deposit(amount, account):
+    db = sqlite3.connect('Bank.db')
 
     cursor = db.cursor()
-    cursor.execute('INSERT INTO savings(total)VALUES(365)')
     db.commit()
 
     db.close()
 
 def withdraw():
     pass
+
+def output():
+    db = sqlite3.connect('Bank.db')
+    cursor = db.cursor()
+    db1 = cursor.execute('SELECT name FROM Accounts')
+    for row in db1:
+        print 'Account: ', row[0]
+    db2 = cursor.execute('SELECT amount FROM Transactions')
+    for row in db2:
+        print 'Amount: ', row[0]
+    # cursor.execute('SELECT * FROM Accounts')
+    # p = cursor.fetchall()
+    # for x in p:
+    #     print x
+    db.close()
--- test_calc.py
+++ test_calc.py
@@ -1,8 +1,17 @@
 import unittest
+import calc
 
-class TestDeposit(unittest.TestCase):
-	def test_amount(self):
+class TestCalc(unittest.TestCase):
+	def test_create_account(self):
+		calc.create_account('saving', 700)
 		self.assertTrue(1)
+
+	def test_deposit(self):
+		calc.deposit(333, 'saving')
+		self.assertTrue(1)
+
+	def test_output(self):
+		calc.output()
 
 if __name__ == '__main__':
 	unittest.main()
