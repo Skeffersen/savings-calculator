d7e62ec7de200c9107ce53af57d686cb6b71143d
Derek Kadwell <derek.kadwell@gmail.com>
2014-10-30 20:33:08 -0600

    completed first version of bank account app. need to fix test in test_calc, all assert true without testing anything

--- calc.py
+++ calc.py
@@ -1,12 +1,11 @@
 import sqlite3
 
-def create_account(account_name='saving', initial_deposit=0):
+def create_account(account_name='saving'):
     db = sqlite3.connect('Bank.db')
 
     cursor = db.cursor()
-    cursor.execute('CREATE TABLE IF NOT EXISTS Transactions(id INTEGER PRIMARY KEY, account_id INTEGER, type TEXT, amount INTEGER)')
-    cursor.execute('INSERT INTO Transactions(account_id, type, amount)VALUES(1, ?, ?)', ('Deposit', initial_deposit))
-    cursor.execute('CREATE TABLE IF NOT EXISTS Accounts(id INTEGER PRIMARY KEY, name TEXT)')
+    cursor.execute('CREATE TABLE IF NOT EXISTS Transactions(transactionID INTEGER PRIMARY KEY, accountID INTEGER, type TEXT, amount INTEGER)')
+    cursor.execute('CREATE TABLE IF NOT EXISTS Accounts(accountID INTEGER PRIMARY KEY, name TEXT)')
     cursor.execute('INSERT INTO Accounts(name)VALUES(?)', (account_name,))
     db.commit()
 
@@ -16,24 +15,48 @@ def deposit(amount, account):
     db = sqlite3.connect('Bank.db')
 
     cursor = db.cursor()
+    cursor.execute('SELECT accountID FROM Accounts WHERE Accounts.name = (?)', (account,))
+    ACCOUNT = cursor.fetchone()[0]
+    cursor.execute('INSERT INTO Transactions(accountID, type, amount)VALUES(?, ?, ?)', (ACCOUNT, 'Deposit', amount))
     db.commit()
 
     db.close()
 
 def withdraw():
-    pass
+    db = sqlite3.connect('Bank.db')
+
+    cursor = db.cursor()
+    db.commit()
+
+    db.close()
 
 def output():
     db = sqlite3.connect('Bank.db')
     cursor = db.cursor()
-    db1 = cursor.execute('SELECT name FROM Accounts')
+    db1 = cursor.execute('SELECT name, accountID FROM Accounts')
     for row in db1:
         print 'Account: ', row[0]
-    db2 = cursor.execute('SELECT amount FROM Transactions')
+        print 'accountID: ', row[1]
+        print '---------------'
+    db2 = cursor.execute('SELECT amount, transactionID, accountID FROM Transactions')
     for row in db2:
         print 'Amount: ', row[0]
+        print 'transactionID: ', row[1]
+        print 'accountID: ', row[2]
     # cursor.execute('SELECT * FROM Accounts')
     # p = cursor.fetchall()
     # for x in p:
     #     print x
+    db.close()
+
+def transaction_receipt():
+    db = sqlite3.connect('Bank.db')
+
+    cursor = db.cursor()
+    transaction_history = cursor.execute('SELECT amount, transactionID, accountID FROM Transactions')
+    for row in transaction_history:
+        print 'Amount: ', row[0]
+        print 'transactionID: ', row[1]
+        print 'accountID: ', row[2]
+
     db.close()
--- test_calc.py
+++ test_calc.py
@@ -3,15 +3,21 @@ import calc
 
 class TestCalc(unittest.TestCase):
 	def test_create_account(self):
-		calc.create_account('saving', 700)
+		calc.create_account('savings')
+		calc.create_account('debt')
 		self.assertTrue(1)
 
 	def test_deposit(self):
-		calc.deposit(333, 'saving')
+		calc.deposit(3900, 'debt')
 		self.assertTrue(1)
 
 	def test_output(self):
 		calc.output()
+		self.assertTrue(1)
+
+	def test_transaction_receipt(self):
+		calc.transaction_receipt()
+		self.assertTrue(1)
 
 if __name__ == '__main__':
 	unittest.main()
